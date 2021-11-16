983a12f69766abd0a8d14d2e2e6de112c5086347
Fidel Lascano <fhalcom.tech@gmail.com>
2021-11-16 10:56:28 -0500

    Add: Secretario

---
+++ src/main/java/es/pildoras/IoC/SecretarioEmpleado.java
@@ -0,0 +1,8 @@
+package main.java.es.pildoras.IoC;
+
+public class SecretarioEmpleado implements EmpleadosI{
+    public String getTareas()
+    {
+        return "Gestionado las tareas de los jefes";
+    }
+}
