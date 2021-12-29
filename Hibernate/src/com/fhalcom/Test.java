7c4e0a998d54756a613ca5bf565e3f05841ef68b
Fidel Lascano <fidel.lascano@conauto.com.ec>
2021-12-29 15:59:19 -0500

    Hibernate: Database connection

---
+++ Hibernate/src/com/fhalcom/Test.java
@@ -0,0 +1,23 @@
+package com.fhalcom;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Test{
+    public static void main(String[] args) {
+        //jdbc:driver://ip:port/dbname
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/fhalcom";
+        String user = "postgres";
+        String pass = "Df3a0b951";
+        try 
+        {
+            System.out.println("DB connecting...");
+            Connection conn = DriverManager.getConnection(jdbcUrl, user, pass);
+            System.out.println("DB is connected.. ");
+            conn.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("DB connection Error...");
+        }
+    }
+}
